525a45b478db37ccafb299301ca12967b8023748
qiaofeng1227 <76487013@qq.com>
2022-06-10 08:52:54 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,6 +3,6 @@
 echo $DB_MYSQL_PASSWORD
 
 sed -i "s/127.0.0.1/db/g" /usr/src/discuz/upload/config/config_global_default.php
-sed -i "34{s/=.*/= '$DB_MYSQL_PASSWORD';/}"  /usr/src/discuz/upload/config/config_global_default.php
+sed -i "34{s/=.*/= '$DISCUZ_DB_PASSWORD';/}"  /usr/src/discuz/upload/config/config_global_default.php
 
 /entrypoint.sh apache2-foreground
